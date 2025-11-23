96e3c4f597a84b4e05a50bc94feffc7a0f52321c
max-shelll <max-shelll@proton.me>
2025-11-23 16:32:58 +0300

    Update

--- Game/Task.c
+++ Game/Task.c
@@ -7,32 +7,79 @@
 #define BLACK 0.3
 #define NUMBER 0.6
 
-char board[SIZE][SIZE];
-
-void initializeBoard();
-void printBoard();
-int isValidBoard();
-void saveToFile();
-void generateGameBoard();
+void initializeBoard(char* board[SIZE][SIZE]);
+void printBoard(char* board[SIZE][SIZE]);
+int isValidBoard(char* board[SIZE][SIZE]);
+int saveToFile(char* board[SIZE][SIZE], char* fileName);
+void log_error(char* message);
 
 int main() {
     setlocale(LC_CTYPE, "RUS");
     srand(time(NULL));
 
-    int choice = 1;
-    do {
-        generateGameBoard();
-        printBoard();
-        saveToFile();
+    char* board[SIZE][SIZE];
+    char* fileName[200];
+    
+    int choice = 0;
+    printf("[-------- Game Light Up --------]\n");
+    printf("| 1) Сгенерировать поле         |\n");
+    printf("| 2) Сохранить поле в файл      |\n");
+    printf("| 3) Узнать правила игры        |\n");
+    printf("| 4) Выход                      |\n");
+    printf("[-------------------------------]\n");
 
-        printf("\nНачать новую генерацию поля (1 - да, 0 - нет)?\n");
+    while (1 > 0) {
+        printf("\nВведите значение: ");
         scanf("%d", &choice);
-    } while (choice != 0);
+
+        switch (choice) {
+            case 1:
+                do {
+                    initializeBoard(board);
+                } while (!isValidBoard(board));
+
+                printBoard(board);
+
+                break;
+            case 2:
+                printf("Введите название файла (ex: map.txt): ");
+                scanf("%s", &fileName);
+
+                int res = saveToFile(board, fileName);
+                if (res == 0)
+                    printf("Поле сохранено в файл: %s\n", fileName);
+                else {
+                    printf("ОШИБКА! Посмотрите файл ошибок\n");
+                    log_error("Файл для сохранения поля не был создан");
+                }
+
+                break;
+            case 3:
+                printf("[------------------------ Rule Light Up ------------------------]\n");
+                printf("| > Размещайте ламы на белых клетках, чтобы осветить все поле   |\n");
+                printf("| > Лампы светят по горизонтали и вертикали до черных клеток    |\n");
+                printf("| > Лампы не должны светить друг на друга                       |\n");
+                printf("| > Числа в черных клетках = количеству соседних ламп           |\n");
+                printf("[---------------------------------------------------------------]\n");
+
+                break;
+            case 4:
+                exit(0);
+                break;
+            default:
+                printf("Команда `%d` не найдена\n", choice);
+                break;
+        }
+    }
 
     return 0;
 }
 
-void initializeBoard() {
+/**
+* Заполнение поля значениями
+* @param board - поле для заполнения
+*/
+void initializeBoard(char* board[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if ((double)rand() / RAND_MAX < BLACK) {
@@ -55,7 +102,11 @@ void initializeBoard() {
     }
 }
 
-void printBoard() {
+/**
+* Печать поля
+* @param board - поле для печати
+*/
+void printBoard(char* board[SIZE][SIZE]) {
     printf("\n");
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
@@ -65,7 +116,12 @@ void printBoard() {
     }
 }
 
-int isValidBoard() {
+/**
+* Проверка значений поля на корректность
+* @param board - проверяемое поле
+* @return успешность генерации (0 - ошибка, 1 - успешно)
+*/
+int isValidBoard(char* board[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
             if (board[i][j] >= '1' && board[i][j] <= '3') {
@@ -103,27 +159,33 @@ int isValidBoard() {
     return 1;
 }
 
-
-void saveToFile() {
-    FILE* file = fopen("lightup_board.txt", "w");
-    if (file == NULL) {
-        printf("Ошибка создания файла!\n");
-        return;
-    }
+/**
+* Сохранение поля в файл
+* @param board - поле для сохранения
+* @param fileName - имя файла для сохранения
+* @return успешность сохранения (-1 - ошибка, 0 - успешно)
+*/
+int saveToFile(char* board[SIZE][SIZE], char* fileName) {
+    FILE* file = fopen(fileName, "w");
+    if (file == NULL) return -1;
 
     for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            fprintf(file, "%c", board[i][j]);
-        }
+        for (int j = 0; j < SIZE; j++) fprintf(file, "%c", board[i][j]);
         fprintf(file, "\n");
     }
 
     fclose(file);
-    printf("\nПоле сохранено в файл: lightup_board.txt\n");
+    return 0;
 }
 
-void generateGameBoard() {
-    do {
-        initializeBoard();
-    } while (!isValidBoard());
+/**
+* Сохранение ошибки в файл
+* @param message - сообщение ошибки
+*/
+void log_error(char* message) {
+    FILE* log_file = fopen("errors.txt", "a");
+    if (log_file) {
+        fprintf(log_file, "ERROR: %s\n", message);
+        fclose(log_file);
+    }
 }
